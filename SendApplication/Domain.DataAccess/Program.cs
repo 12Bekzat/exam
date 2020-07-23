4f9e7adcb1863aca0a8f78ccb63be8c14129da27
12Bekzat <toleutaib@gmail.com>
2020-07-23 20:17:50 +0600

    Создал проект

---
+++ SendApplication/Domain.DataAccess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domain.DataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
